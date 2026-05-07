bc4fad054cccc6ee3bfa61020c606e2e4e73cc9a
Nafiz Himel <nafizhimel7@gmail.com>
2026-05-07 17:25:59 +0600

    added-files

---
+++ src/redux/pasteSlice.jsx
@@ -0,0 +1,31 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+  paste: localStorage.getItem('paste')
+  ? JSON.parse(localStorage.getItem('paste'))
+  : []
+}
+
+export const pasteSlice = createSlice({
+  name: 'paste',
+  initialState,
+  reducers: {
+    addToPaste: (state, action) => {
+
+    },
+    updateToPaste: (state, action) => {
+      
+    },
+    resetAllPaste: (state, action) => {
+      
+    },
+    removeFromPaste: (state, action) => {
+      
+    },
+  },
+})
+
+// Action creators are generated for each case reducer function
+export const { addToPaste, updateToPaste, resetAllPaste, removeFromPaste } = pasteSlice.actions
+
+export default pasteSlice.reducer
